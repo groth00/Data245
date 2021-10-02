7ee32930ab2dc9215968c42f5b73b03840ca5880
groth00 <busygroth@gmail.com>
2021-10-01 23:17:06 -0700

    v3
    VotingClassifier, StackingClassifier, some StratifiedShuffleSplit (cv)

--- models_testing.py
+++ models_testing.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import numpy as np
 from time import time
 
-from sklearn.model_selection import train_test_split, KFold, cross_validate, \
+from sklearn.model_selection import train_test_split, cross_validate, \
     StratifiedShuffleSplit
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression, SGDClassifier
@@ -14,11 +14,11 @@ from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import RandomForestClassifier, \
     HistGradientBoostingClassifier, VotingClassifier, StackingClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import ComplementNB
 from sklearn.metrics import confusion_matrix, classification_report, \
     recall_score
 
-from imblearn.under_sampling import RandomUnderSampler, TomekLinks, \
-    EditedNearestNeighbours
+from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.ensemble import BalancedBaggingClassifier, \
     BalancedRandomForestClassifier, EasyEnsembleClassifier
@@ -268,30 +268,213 @@ imbalanced_ensemble_predictions = scoreMultipleEstimators(
 #  0.6693237707193055
 
 
+# %%
+def buildPipelines(estimator_dict,
+                   sampler = RandomUnderSampler(random_state = 111)):
+    '''
+    takes in dictionary
+    k = name of estimator
+    v = estimator object
+    
+    returns list of imbalanced-learn pipelines w/MinMaxScaler, RUS, estimator
+    
+    you can pass this directly into a VotingClassifier
+    '''
+    pipelines = [
+        imblearn_pipeline(MinMaxScaler(), sampler, estimator) \
+            for estimator in estimator_dict.values()
+        ]
+    return list(zip(estimator_dict.keys(), pipelines))
 
-# %% new: VotingClassifier and StackedClassifier
-individual_pipelines = [imblearn_pipeline(MinMaxScaler(), 
-                   RandomUnderSampler(random_state = 111),
-                   estimator) for estimator in estimator_dict.values()]
+def fitVotingClassifier(voting_classifier, X_train, y_train):
+    print('Started training.\n')
+    start = time()
+    voting_classifier.fit(X_train, y_train)
+    end = time()
+    print(f'{end - start} seconds elapsed.\n')
+    return voting_classifier
 
-# majority vote (hard)
-# uniform weights for each classifier
-v = VotingClassifier(estimators = list(zip(
-    [k for k in estimator_dict.keys()], individual_pipelines)),
-                     voting = 'hard',
-                     weights = None,
-                     n_jobs = -1,
-                     verbose = True)
+def scoreVotingClassifier(voting_classifier, X_test, y_test):
+    predictions = voting_classifier.predict(X_test)
+    print(f'Recall is {recall_score(y_test, predictions)}')
+    return None
 
-v.fit(X_train, y_train)
-voter_predictions = v.predict(X_test)
 
-# better than DT, SVC, slightly better than LR, but worse than SGDC and MLP
-print(recall_score(y_test, voter_predictions))
-# 0.6375606452544048
+# %% VotingClassifier - individual models, hard vote 
+individual_models = {
+    'LR': LogisticRegression(max_iter = 100, 
+                             n_jobs = -1, 
+                             random_state = 111,
+                             solver = 'saga'),
+    'DT': DecisionTreeClassifier(random_state = 111),
+    'ANN': MLPClassifier(learning_rate = 'invscaling',
+                         random_state = 111,
+                         early_stopping = True),
+    'SVC': LinearSVC(dual = False,
+                     random_state = 111,
+                     max_iter = 1000),
+    'SGDC': SGDClassifier(max_iter = 1000, verbose = 0,
+                          n_jobs = -1, random_state = 111,
+                          learning_rate = 'optimal',
+                          early_stopping = True,
+                          )}
+pipelines = buildPipelines(individual_models)
+model = VotingClassifier(estimators = pipelines,
+                              voting = 'hard',
+                              n_jobs = -1)
+model_fit = fitVotingClassifier(model, X_train, y_train)
+scoreVotingClassifier(model_fit, X_test, y_test)
 
-# ---------------------------------------------------------------------
-# %% trying a train_size of 0.25 + StratifiedShuffleSplit
+# Recall is 0.6375606452544048
+
+# %% VotingClassifier - individual models + ensembles, hard vote
+individual_and_ensemble = {
+    'LR': LogisticRegression(max_iter = 100, 
+                             n_jobs = -1, 
+                             random_state = 111,
+                             solver = 'saga'),
+    'DT': DecisionTreeClassifier(random_state = 111),
+    'ANN': MLPClassifier(learning_rate = 'invscaling',
+                         random_state = 111,
+                         early_stopping = True),
+    'SVC': LinearSVC(dual = False,
+                     random_state = 111,
+                     max_iter = 1000),
+    'SGDC': SGDClassifier(max_iter = 1000, verbose = 0,
+                          n_jobs = -1, random_state = 111,
+                          learning_rate = 'optimal',
+                          early_stopping = True,
+                          ),
+    'HGBC': HistGradientBoostingClassifier(random_state = 111),
+    'RF': RandomForestClassifier(
+        n_estimators = 100, max_depth = None, bootstrap = True,
+        oob_score = True, n_jobs = -1, random_state = 111,
+        max_samples = 0.5)
+    }
+
+pipelines_all = buildPipelines(individual_and_ensemble)
+v_all = VotingClassifier(estimators = pipelines_all,
+                         voting = 'hard', 
+                         weights = None,
+                         n_jobs = -1)
+v_all_fit = fitVotingClassifier(v_all, X_train, y_train)
+scoreVotingClassifier(v_all_fit, X_test, y_test)
+
+# 322.4073898792267 seconds elapsed.
+# Recall is 0.6518490557658788
+
+# %% VotingClassifier - individual models, soft vote
+vc_sv = {
+    'ANN': MLPClassifier(learning_rate = 'invscaling',
+                         random_state = 111,
+                         early_stopping = True),
+    'LR': LogisticRegression(max_iter = 100, 
+                             n_jobs = -1, 
+                             random_state = 111,
+                             solver = 'saga'),
+    'DT': DecisionTreeClassifier(random_state = 111),
+    'NB': ComplementNB(),
+    'SGDC': SGDClassifier(loss = 'modified_huber',
+                          max_iter = 1000, verbose = 0,
+                          n_jobs = -1, random_state = 111,
+                          learning_rate = 'optimal',
+                          early_stopping = True,
+                          )}
+
+pipelines_sv = buildPipelines(vc_sv)
+sv = VotingClassifier(estimators = pipelines_sv,
+                      voting = 'soft', 
+                      weights = None, 
+                      n_jobs = -1)
+sv_fit = fitVotingClassifier(sv, X_train, y_train)
+scoreVotingClassifier(sv_fit, X_test, y_test)
+
+# 0.6168330132226083
+
+# %% VC - individual + ensemble models, soft vote
+vc_sv_all = {
+    'LR': LogisticRegression(max_iter = 100, 
+                             n_jobs = -1, 
+                             random_state = 111,
+                             solver = 'saga'),
+    'DT': DecisionTreeClassifier(random_state = 111),
+    'ANN': MLPClassifier(learning_rate = 'invscaling',
+                         random_state = 111,
+                         early_stopping = True),
+    'NB': ComplementNB(),
+    'SGDC': SGDClassifier(loss = 'modified_huber',
+                          max_iter = 1000, verbose = 0,
+                          n_jobs = -1, random_state = 111,
+                          learning_rate = 'optimal',
+                          early_stopping = True,
+                          ),
+    'HGBC': HistGradientBoostingClassifier(random_state = 111),
+    'RF': RandomForestClassifier(
+        n_estimators = 100, max_depth = None, bootstrap = True,
+        oob_score = True, n_jobs = -1, random_state = 111,
+        max_samples = 0.5)
+    }
+
+pipelines_sv_all = buildPipelines(vc_sv_all)
+sv_all = VotingClassifier(estimators = pipelines_sv_all,
+                          voting = 'soft', 
+                          weights = None, 
+                          n_jobs = -1)
+sv_all_fit = fitVotingClassifier(sv_all, X_train, y_train)
+scoreVotingClassifier(sv_all_fit, X_test, y_test)
+
+# 340.994647026062 seconds elapsed.
+# Recall is 0.6359841461925327
+
+
+# %% StackingClassifier is very slow (16-17 minutes)
+sc_dict = {
+    'DT': DecisionTreeClassifier(random_state = 111),
+    'ANN': MLPClassifier(learning_rate = 'invscaling',
+                         random_state = 111,
+                         early_stopping = True),
+    'SGDC': SGDClassifier(max_iter = 1000, verbose = 0,
+                          n_jobs = -1, random_state = 111,
+                          learning_rate = 'optimal',
+                          early_stopping = True,
+                          ),
+    'HGBC': HistGradientBoostingClassifier(random_state = 111),
+    'RF': RandomForestClassifier(
+        n_estimators = 100, max_depth = None, bootstrap = True,
+        oob_score = True, n_jobs = -1, random_state = 111,
+        max_samples = 0.5),
+    'LR': LogisticRegression(max_iter = 100, 
+                             n_jobs = -1, 
+                             random_state = 111,
+                             solver = 'saga')
+    }
+
+sc_pipelines = [
+    imblearn_pipeline(MinMaxScaler(), 
+    RandomUnderSampler(random_state = 111), 
+    estimator) \
+        for estimator in sc_dict.values()
+    ]
+
+s = StackingClassifier(
+    estimators = list(zip(list(sc_dict.keys())[:-1], sc_pipelines[:-1])),
+    final_estimator = sc_pipelines[-1],
+    cv = 4,
+    stack_method = 'auto',
+    n_jobs = -1,)
+
+start = time()
+s.fit(X_train, y_train)
+end = time()
+print(f'{end-start} seconds')
+sc_predictions = s.predict(X_test)
+print(recall_score(y_test, sc_predictions))
+
+# 941.7893340587616 seconds
+# 0.6783053745295483
+
+
+# %% StratifiedShuffleSplit w/a train_size of 0.25, individual models
 
 ind_cv = fitMultipleEstimatorsWithCV(
     X, y, estimator_dict, 
@@ -301,37 +484,27 @@ ind_cv = fitMultipleEstimatorsWithCV(
                                 random_state = 111))
 
 # Started training LR with RandomUnderSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6348872801027452 +/- 0.0005213785620505781
 # Finished training LR in 20.357626914978027 seconds. 
 
 # Started training DT with RandomUnderSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.5642379424316111 +/- 0.000916049529759271
 # Finished training DT in 20.305978298187256 seconds. 
 
 # Started training ANN with RandomUnderSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6691455420955427 +/- 0.011965077509234824
 # Finished training ANN in 151.715106010437 seconds. 
 
-# Started training SVC with RandomUnderSampler(random_state=111).
-# Average recall and standard deviation 
+# Started training SVC with RandomUnderSampler(random_state=111). 
 # 0.6315913658520336 +/- 0.0005496453884532532
 # Finished training SVC in 15.03044319152832 seconds. 
 
 # Started training SGDC with RandomUnderSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6759511146149092 +/- 0.03589493448214965
 # Finished training SGDC in 13.092419147491455 seconds.
 
-# ---------------------------------------------------------------------
-# %% other sampling techniques
-# tried TomekLinks, ENN with 0.25/0.75, didn't finish
-# tried with 0.1/0.9, didn't finish either
-# works quickly with 0.01/0.99, but it's pointless 
 
-# RandomUnderSampler > RandomOverSampler in terms of recall and time
+# %% Same as cell above but using RandomOverSampler
 ind_os_cv = fitMultipleEstimatorsWithCV(
     X, y, estimator_dict,
     sampler = RandomOverSampler(random_state = 111),
@@ -340,26 +513,21 @@ ind_os_cv = fitMultipleEstimatorsWithCV(
                                 random_state = 111))
 
 # Started training LR with RandomOverSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6351935539985861 +/- 0.0005493345411437624
 # Finished training LR in 48.57807731628418 seconds. 
 
 # Started training DT with RandomOverSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.26963761598620195 +/- 0.0017555558910426583
 # Finished training DT in 52.16799807548523 seconds. 
 
 # Started training ANN with RandomOverSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6761731863218632 +/- 0.015141963654061948
 # Finished training ANN in 1103.6012589931488 seconds. 
 
 # Started training SVC with RandomOverSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.6316644644555727 +/- 0.0005961509814442347
 # Finished training SVC in 32.7990562915802 seconds. 
 
 # Started training SGDC with RandomOverSampler(random_state=111).
-# Average recall and standard deviation 
 # 0.5906857574310744 +/- 0.031092631065272963
 # Finished training SGDC in 14.984627962112427 seconds. 
